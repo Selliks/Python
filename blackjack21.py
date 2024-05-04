cffd0e66e2646d706197fe9e84597f6ac0411b57
Dima <150135064+Selliks@users.noreply.github.com>
2024-05-05 00:02:43 +0300

    blackjack21.py

--- blackjack21.py
+++ blackjack21.py
@@ -1,20 +1,22 @@
-import tkinter
 import customtkinter
 import random
 
 #p and c score and deck
+
 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]*4
 score = 0
 computer_score = 0
 
 #check variables
+
 first_function_used = False
 too_much_computer = False
 player_lost = False
 too_much_player = False
-best_game = False
+resultats_used = False
 
 #creating window
+
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("green")
 
@@ -22,17 +24,19 @@ app = customtkinter.CTk()
 app.geometry("800x600")
 
 # Creating Frames
+
 left_frame = customtkinter.CTkFrame(app)
 left_frame.pack(side="left", fill="both", expand=True)
 
 right_frame = customtkinter.CTkFrame(app)
 right_frame.pack(side="right", fill="both", expand=True)
-# PLAYER CHOICES
-# LEFT FRAME
-the_player_label = customtkinter.CTkLabel(left_frame, text="Player choice")
-the_player_label.pack()
+
+
+# LEFT FRAME - PLAYER CHOICES
+
 
 # The function of calling a taking move
+
 def take_black_jack():
     global score 
     global first_function_used
@@ -47,12 +51,10 @@ def take_black_jack():
         all_card_label = customtkinter.CTkLabel(left_frame, text="All your cards now - %d"% score)
         all_card_label.pack()
         if score > 21:
-            defeat_label = customtkinter.CTkLabel(left_frame, text="Defeat! Your score - %d! Don't worry, it's game! "% score)
-            defeat_label.pack()
             too_much_player = True
-        elif score == 21:
-            score21_label = customtkinter.CTkLabel(left_frame, text="Great move! You Won! 21/21")
-            score21_label.pack()  
+    elif score == 21:
+        score21_label_player = customtkinter.CTkLabel(right_frame, text="Player won!")
+        score21_label_player.pack()
     elif player_lost == False:
         blocked_label = customtkinter.CTkLabel(left_frame, text="You've already clicked pass")
         blocked_label.pack()
@@ -61,45 +63,59 @@ def take_black_jack():
 # The function of calling a passing move
 
 def pass_black_jack():
-    global score 
-    global computer_score
     global first_function_used
-    global too_much_computer
-    global best_game
     
-    if too_much_computer == True and best_game == False:
+    if first_function_used == False:
         pass
-    elif computer_score > 21:
-        score_label = customtkinter.CTkLabel(right_frame, text="The computer lost!")
-        score_label.pack()
-        too_much_computer = True
-    elif computer_score <= 21 and score <= 21:
-        if computer_score > score:
-            score_label = customtkinter.CTkLabel(left_frame, text="Defeat! Computer won!")
-            score_label.pack()
-        elif score > computer_score:
-            score_label = customtkinter.CTkLabel(left_frame, text="Best game! You won!")
-            score_label.pack()
-        elif score == computer_score:
-            score_label = customtkinter.CTkLabel(left_frame, text="Draw! Nobody won")
-            score_label.pack()
-    
-    first_function_used = True and best_game == True
+    first_function_used = True
 
-# Buttons for left frame
+# Buttons for Left Frame
+
+the_player_label = customtkinter.CTkLabel(left_frame, text="Player choice")
+the_player_label.pack()
+
 take_button = customtkinter.CTkButton(left_frame, text="Take", command=take_black_jack)
-pass_button = customtkinter.CTkButton(left_frame, text="Pass", command=pass_black_jack)
 take_button.pack()
+
+pass_button = customtkinter.CTkButton(left_frame, text="Pass", command=pass_black_jack)
 pass_button.pack()
 
-# RIGHT FRAME
-the_computer_label = customtkinter.CTkLabel(right_frame, text="Computer choice")
-the_computer_label.pack()
+# RESULTATS
+
+def resultats():
+    global score 
+    global computer_score
+    global resultats_used
+
+    if resultats_used == True:
+        pass
+    elif computer_score <= 21 and score <= 21:
+        if computer_score > score:
+            score_label = customtkinter.CTkLabel(right_frame, text="Defeat! Computer won!")
+            score_label.pack()
+        elif score > computer_score:
+            score_label = customtkinter.CTkLabel(right_frame, text="Best game! You won!")
+            score_label.pack()
+        elif score == computer_score:
+            score_label = customtkinter.CTkLabel(right_frame, text="Draw! Nobody won")
+            score_label.pack()
+    elif computer_score >= 21 and score >= 21:
+        score_label = customtkinter.CTkLabel(right_frame, text="Draw! Nobody won")
+        score_label.pack()
+
+    resultats_used = True
+
+
+# RIGHT FRAME - COMPUTER CHOICES
+
+
 def take_black_jack_computer():
     global computer_score
     global first_function_used
     global too_much_computer
-    if first_function_used == True or too_much_player == True and too_much_computer == False:
+    global resultats_used
+
+    if first_function_used == True and too_much_computer == False and resultats_used == False:
         random.shuffle(deck)
         computer_current = deck.pop()
         card_label = customtkinter.CTkLabel(right_frame, text="Your card - %d"% computer_current)
@@ -108,15 +124,22 @@ def take_black_jack_computer():
         all_card_label = customtkinter.CTkLabel(right_frame, text="All computer cards now - %d"% computer_score)
         all_card_label.pack()
         if computer_score > 21:
-            defeat_label = customtkinter.CTkLabel(right_frame, text="Defeat! Computer score - %d! "% computer_score)
-            defeat_label.pack()
             too_much_computer = True
         elif computer_score == 21:
             score21_label = customtkinter.CTkLabel(right_frame, text="Computer won!")
             score21_label.pack()
     
+# Creating right frame buttons
+
+the_computer_label = customtkinter.CTkLabel(right_frame, text="Computer choice")
+the_computer_label.pack()
 
 take_button_computer = customtkinter.CTkButton(right_frame, text="Take", command=take_black_jack_computer)
 take_button_computer.pack()
+
+result_button = customtkinter.CTkButton(right_frame, text="Result", command=resultats)
+result_button.pack()
+
+# Starting!
 
 app.mainloop()
